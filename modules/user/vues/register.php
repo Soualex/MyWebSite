f0d13ee22db37ebe8503d793856a131ef6894e3e
Soualex <soualexduseptsix@gmail.com>
2013-04-17 17:56:19 +0200

    Modification des varaiables d'erreur

--- modules/user/vues/register.php
+++ modules/user/vues/register.php
@@ -1,21 +1,21 @@
-<form method="post" action="index.php?module=user&action=register" enctype="multipart/form-data">
+<form method="post" action="index.php?module=user&action=register" enctype="multipart/form-data" class="page" >
     <p><strong><a href="./index.php"><img src="style/image/home.png" class="icon" /></a>: Inscription</strong></p>
     <h2><img src="style/image/emerald.png" class="icon" />Inscription</h2>
             
     <fieldset><legend>Identifiants</legend>
         <label for="reg_pseudo">Pseudo :</label>  <input name="reg_pseudo" type="text" id="reg_pseudo" required autofocus />
-        <?php if (isset($register->err_pseudo)) echo '<img src="style/image/alert.png" class="icon" title="'.$register->err_pseudo.'" />'; ?><br />
+        <?php if (isset($errors['pseudo'])) echo '<img src="style/image/alert.png" class="icon" title="'.$errors['pseudo'].'" />'; ?><br />
                     
         <label for="password">Mot de Passe :</label><input type="password" name="password" id="password" required />
-        <?php if (isset($register->err_pass)) echo '<img src="style/image/alert.png" class="icon" title="'.$register->err_pass.'" />'; ?><br />
+        <?php if (isset($errors['password'])) echo '<img src="style/image/alert.png" class="icon" title="'.$errors['password'].'" />'; ?><br />
                     
         <label for="confirm">Confirmation :</label><input type="password" name="confirm" id="confirm" required />
-        <?php if (isset($register->err_confirm_pass)) echo '<img src="style/image/alert.png" class="icon" title="'.$register->err_confirm_pass.'" />'; ?><br />
+        <?php if (isset($errors['confirm_password'])) echo '<img src="style/image/alert.png" class="icon" title="'.$errors['confirm_password'].'" />'; ?><br />
     </fieldset>
             
     <fieldset><legend>Contacts</legend>
         <label for="email">Adresse E-Mail :</label><input type="text" name="email" id="email" required />
-        <?php if (isset($register->err_email)) echo '<img src="style/image/alert.png" class="icon" title="'.$register->err_email.'" />'; ?><br />
+        <?php if (isset($errors['email'])) echo '<img src="style/image/alert.png" class="icon" title="'.$errors['email'].'" />'; ?><br />
     </fieldset>
             
     <p><center><input type="submit" value="S'inscrire" class="btn btn-green"></center></p>
