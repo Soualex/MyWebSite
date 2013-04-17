09cc9cd143f2b495ce6f6fc03740b47ef3cd3def
Soualex <soualexduseptsix@gmail.com>
2013-04-17 17:51:03 +0200

    Ajout de constantes

--- global/config.php
+++ global/config.php
@@ -4,7 +4,16 @@
 define('SQL_DSN',      'mysql:dbname=minecraft;host=localhost');
 define('SQL_USERNAME', 'root');
 define('SQL_PASSWORD', '');
- 
+
+// Constantes
+define('ERR_IS_CO','Vous ne pouvez pas accéder à cette page si vous êtes connecté');
+define('ERR_IS_NOT_CO','Vous ne pouvez pas accéder à cette page si vous n\'êtes pas connecté');
+define('ALREADY_LOGIN','Vous êtes déjà connecté');
+define('ALREADY_REGISTRED','Vous êtes déjà inscrit');
+define('EMPTY_FIELD','Veulliez remplir ce champ');
+define('CHAR_FORBIDDEN','Ce champs contient des caractères interdits');
+define('LOW_LEVEL','Vous n\'avez pas un rang assez élevé pour acceder à cette');
+
 // Chemins à utiliser pour accéder aux vues/modeles/librairies
 $module = empty($module) ? !empty($_GET['module']) ? $_GET['module'] : 'index' : $module;
 define('VUE_DIR',    'modules/'.$module.'/vues/');
