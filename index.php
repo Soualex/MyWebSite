5ee7b3ade0e8e9264d7d18459d61034a4393416f
Soualex <soualexduseptsix@gmail.com>
2013-04-18 00:23:42 +0200

    Optimisation du code de récupération des modules et meilleur gestion des erreurs

--- index.php
+++ index.php
@@ -1,34 +1,41 @@
 <?php
- 
+
 // Initialisation
 require 'global/init.php';
- 
+
 // Début de la tamporisation de sortie
 ob_start();
  
 // Si un module est specifié, on regarde s'il existe
 if (isset($_GET['module'])) 
 {
-    $module = dirname(__FILE__).'/modules/'.$_GET['module'].'/';
-     
-    // Si l'action est specifiée, on l'utilise, sinon, on tente une action par défaut
-    $action = (!empty($_GET['action'])) ? $_GET['action'].'.php' : 'index.php';
-     
-    // Si l'action existe, on l'exécute
-    if (is_file($module.$action)) 
-    {
-        include $module.$action;
+    if ($_GET['module'] == is_dir(dirname(__FILE__).'/modules/'.$_GET['module'].'/')) 
+    {       
+        $module = dirname(__FILE__).'/modules/'.$_GET['module'].'/';
+         
+        // Si l'action est specifiée, on l'utilise, sinon, on tente une action par défaut
+        $action = (!empty($_GET['action'])) ? $_GET['action'].'.php' : 'index.php';
+         
+        // Si l'action existe, on l'exécute
+        if (is_file($module.$action)) 
+        {
+            include $module.$action;
+        }
+        else 
+        {
+            erreur('Action Impossible', 'Cette action ne vous est pas autorisé ou est inexistante');
+        }
     }
-    else 
+    else
     {
-        include 'modules/news/news.php';
+        erreur('Page Inexistante', 'La page que vous tentez d\'afficher est introuvable sur le serveur. Veuille vérfifier les données que vous avez saisi');
     }
-} 
+}
 else 
 {
     include 'modules/news/news.php';
 }
- 
+
 // Fin de la tamporisation de sortie
 $contenu = ob_get_clean();
  
