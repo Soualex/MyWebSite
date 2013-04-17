1bbd09890da198604e9c7434a36f0846b62538c4
Soualex <soualexduseptsix@gmail.com>
2013-04-17 20:41:20 +0200

    Ajout de la fonction de gestion d'erreurs

--- global/init.php
+++ global/init.php
@@ -12,4 +12,20 @@ $user_rank = (isset($_SESSION['rank'])) ? (int) $_SESSION['rank'] : 1;
 $user_id = (isset($_SESSION['id'])) ? (int) $_SESSION['id'] : 0;
 $user_name = (isset($_SESSION['name'])) ? $_SESSION['name'] : '';
 
+// Fonctions
+function erreur($title, $message)
+{
+    if($message == NULL)
+    {
+        $message = 'Une erreur inconnue s\'est produite';
+    }
+    
+    echo '
+    <div class="page">
+        <h2><img src="style/image/erreur.png"  class="icon" />Erreur : '.$title.'</h2>
+        <p>'.$message.'. S\'il s\'agit d\'une erreur, veuillez contacter l\'Administrateur.</p>
+        <p><a href="index.php">Retourner à l\'acceuil</a></p>
+    </div>';
+}
+
 ?>
