38ebfd7854b6b2f66e96e3c29fd32171803f264a
Soualex <soualexduseptsix@gmail.com>
2013-04-19 15:54:22 +0200

    Conversion UTF-8

--- global/init.php
+++ global/init.php
@@ -1,13 +1,13 @@
-<?php
+ï»¿<?php
  
 // Inclusion du fichier de configuration et du loader de .class
 require 'global/config.php';
 require 'libs/autoload.inc.php';
 
-// Utilisation et démarrage des sessions
+// Utilisation et dÃ©marrage des sessions
 session_start();
 
-// Définition des variables de l'utilisateur
+// DÃ©finition des variables de l'utilisateur
 $user_rank = (isset($_SESSION['rank'])) ? (int) $_SESSION['rank'] : 1;
 $user_id = (isset($_SESSION['id'])) ? (int) $_SESSION['id'] : 0;
 $user_name = (isset($_SESSION['name'])) ? $_SESSION['name'] : '';
@@ -24,7 +24,7 @@ function erreur($title, $message)
     <div class="page">
         <h2><img src="style/image/erreur.png"  class="icon" />Erreur : '.$title.'</h2>
         <p>'.$message.'. S\'il s\'agit d\'une erreur, veuillez contacter l\'Administrateur.</p>
-        <p><a href="index.php">Retourner à l\'acceuil</a></p>
+        <p><a href="index.php">Retourner Ã  l\'acceuil</a></p>
     </div>';
 }
 
